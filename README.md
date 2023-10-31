31b67b517d1e6fd75f0fa270591e76d1ff8fc422
Juan Diego <termijow@gmail.com>
2023-10-31 12:52:50 -0500

    Primer Commit
    Commit inicial en el que se conecta la base de datos (sin nada), y las configuraciones basicas del servidor

---
+++ README.md
@@ -0,0 +1,23 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+[circleci-image]: https://img.shields.io/circleci/build/github/nestjs/nest/master?token=abc123def456
+[circleci-url]: https://circleci.com/gh/nestjs/nest
+
+# Deploy (develop)
+
+
+### 1. Install npm packages
+```
+npm install
+```
+### 2. Start docker database
+```
+docker-compose up -d
+```
+### 3. Start server developer mode
+```
+npm run start:dev
+```
+
---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('main')
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    })
+    );
+
+    const config = new DocumentBuilder()
+    .setTitle('llanomarkets') // nombre del proyecto
+    .setDescription('llanomarkets')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
+  logger.log(`app running here http://localhost:${process.env.PORT}`)
+}
+bootstrap();
